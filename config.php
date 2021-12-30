20d2df2163a75a2494165428dd65bb6c5896f222
Adedokun Abdul-Baaki <51439371+adedokun2016@users.noreply.github.com>
2021-12-30 21:55:14 +0200

    create config file
    Upload config file

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'adedokun2016');
+define('DB_PASSWORD', '#Gotohell19955');
+define('DB_NAME', 'adedokun2016');
+ 
+/* Attempt to connect to MySQL database */
+$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($con === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+?>
